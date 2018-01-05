b478da458db92755619474df3c7f429d69354a4f
Phil Mok <plmok61@gmail.com>
2018-01-04 19:42:16 -0600

    Adds random bros

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Home from './components/Home.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Spot Me Bro</h1>
-        </header>
+      <div className="appContainer">
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bro: 'Bro'
+    };
+    this.bros = [
+      'Bro',
+      'Bromosapien',
+      'Bro Montana',
+      'Broseph Stalin',
+      'Bromosaurus',
+      'Broba Fett',
+      'Brofessor',
+      'Brogue One',
+      'Brohemian Rhapbrody',
+      'Brona Lisa',
+      'Brodo Baggins',
+      'Bronan the Barbarian',
+      'Brostradamus',
+    ];
+    this.newBro = this.newBro.bind(this);
+  }
+
+  componentDidMount() {
+    this.newBro()
+  }
+
+  newBro() {
+    setInterval(() => {
+      const index = Math.floor(Math.random() * this.bros.length);
+      const newBro = this.bros[index];
+      this.setState({ bro: newBro });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Spot Me {this.state.bro}</h1>
+      </div>
+    )
+  }
+}
+
+export default Home;
